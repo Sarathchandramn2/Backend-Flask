1ca45866146103763eb1d3b1bdf3bd881ace2b62
Sarathchandramn2 <109575830+Sarathchandramn2@users.noreply.github.com>
2023-01-25 19:28:11 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -1,15 +1,16 @@
 from app import app
-
 from flaskext.mysql import MySQL
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 mydb = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-
-app.config['MYSQL_DATABASE_DB'] = 'indproject'
-
+app.config['MYSQL_DATABASE_DB'] = 'Indproject'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['SECRET_KEY']=True
 
+
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 mydb.init_app(app)
+jwt = JWTManager(app)
