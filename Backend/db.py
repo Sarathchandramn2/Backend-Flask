295437eedf5212c19810ad099691f537bcbe6ec5
Sarathchandramn2 <109575830+Sarathchandramn2@users.noreply.github.com>
2023-01-25 19:29:04 +0530

    Update db.py

--- Backend/db.py
+++ Backend/db.py
@@ -3,43 +3,38 @@ import mysql.connector
 mydb = mysql.connector.connect(
 
     host = "localhost",
-
     user = "root",
-
     password = "",
-
     database = "indproject"
 
 )
 mydb_Create_Table_Query = """CREATE TABLE movie
-( movie_id int(10) not null auto_increment ,
-  movie_name varchar(50) not null,
-  movie_genre varchar(50) not null,
+( movieId int(10) not null auto_increment ,
+  movieName varchar(50) not null,
+  movieGenre varchar(50) not null,
   director  varchar(50) not null,
   language varchar(50) not null,
-  CONSTRAINT movie_pk PRIMARY KEY (movie_id)
+  CONSTRAINT movie_pk PRIMARY KEY (movieId)
 )"""
 
 
 
 
-# mydb_Create_Table_Query= """CREATE TABLE user
-# (
-#    user_id int(100) auto_increment not null,
-#    yourname varchar(50) not null,
-#   username varchar(50) not null,
-#   password varchar(50) not null,
-#   CONSTRAINT user_pk PRIMARY KEY (user_id)
+mydb_Create_Table_Query= """CREATE TABLE user
+(
+   userId int(100) auto_increment not null,
+   yourname varchar(50) not null,
+   username varchar(50) not null,
+   password varchar(50) not null,
+   CONSTRAINT user_pk PRIMARY KEY (user_id)
   
   
-#    )"""
+   )"""
 
 
 
 
 
 cursor = mydb.cursor()
-
 result = cursor.execute(mydb_Create_Table_Query)
-
 print(" Table created successfully ")
