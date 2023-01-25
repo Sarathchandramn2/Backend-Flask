86ced5ca9fb348729a7e486564cba2e8240d4c26
Sarathchandramn2 <109575830+Sarathchandramn2@users.noreply.github.com>
2023-01-25 19:27:36 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -4,8 +4,5 @@ from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
-
-app.secret_key=os.urandom(24)
-
+app.secret_key = 'e5b8a5579ec140809c4df31f48c1d4f3'
 CORS(app)
