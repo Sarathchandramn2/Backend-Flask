36d69f2b725e8eb34a30f859aa5fe05fa78a752a
Sarathchandramn2 <109575830+Sarathchandramn2@users.noreply.github.com>
2023-02-03 20:41:05 +0530

    Delete NewBackend directory

--- NewBackend/Testing/main_test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import httpx
-
-
-if __name__ == '__main__':
-    unittest.main()
--- NewBackend/app.py
+++
@@ -1,6 +0,0 @@
-
-from flask import Flask
-import bcrypt
-from flask_cors import CORS
-app = Flask(__name__)
-CORS(app)
